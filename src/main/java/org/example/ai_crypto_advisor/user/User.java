35d931bae70afc2d802f79b2e7ea529c4707c873
orik <orikeni1309@gmail.com>
2025-10-29 09:03:21 +0200

    fix: rename user table for postgres compatibility

--- src/main/java/org/example/ai_crypto_advisor/user/User.java
+++ src/main/java/org/example/ai_crypto_advisor/user/User.java
@@ -1,15 +1,13 @@
 package org.example.ai_crypto_advisor.user;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
 
     @Id
